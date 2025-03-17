ba9453479320e14d5571d30e1caa32c35183627d
wuguangheng <wuguangheng2002@gmail.com>
2025-03-17 11:15:51 +0800

    update

--- .history/addon/prefs_20250316204214.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-pref("enable", true);
-// pref("input", "This is input");
-
-// 添加 RAGFlow 插件所需首选项
-pref("apiKey", "");
-pref("apiUrl", "http://127.0.0.1");
-pref("kbId", "");
